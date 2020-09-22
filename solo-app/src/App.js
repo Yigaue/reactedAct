6fe49e6100b055bfcab9d6556303654a310df183
Lekia <lekiagospel@gmail.com>
2020-09-22 18:01:40 +0100

    component nestling

--- solo-app/src/App.js
+++ solo-app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Monowo from './Monowo';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <Monowo/>
     </div>
   );
 }
---
+++ solo-app/src/Monowo.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Monowo extends Component {
+    render() {
+        return (
+            <div className="monowo">
+                <div>Name: Jesse</div>
+                <div>Age: 40</div>
+                <div>Height: 5.5 ft</div>
+            </div>
+        )
+    }
+}
+
+export default Monowo;
