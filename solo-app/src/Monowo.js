3a08e8f4f0101f9eb89a7aab02e9291f9b11a66f
Lekia <lekiagospel@gmail.com>
2020-09-23 19:19:49 +0100

    conditional rendering

--- solo-app/src/Monowo.js
+++ solo-app/src/Monowo.js
@@ -2,17 +2,47 @@ import React from 'react';
 
 const Monowo = ({students}) => {
 
-   const studentList = students.map(student => {
-    return (   
-        <div className ="monowo" key = {student.id}>
-            <div>Name: {student.name}</div>
-            <div>Age: {student.age}</div>
-            <div>Colour: {student.height}</div>
-        </div>
-    )
-   })
+//    const studentList = students.map(student => {
+//     if(student.age > 23) {
+//         return (   
+//             <div className ="monowo" key = {student.id}>
+//                 <div>Name: {student.name}</div>
+//                 <div>Age: {student.age}</div>
+//                 <div>Colour: {student.height}</div>
+//             </div>
+//         )
+//     } 
+//     return student.age = <div key = {student.id}>Age: Too Old</div>;
+//    })
+//     const studentList =  students.map(student => {
+//         return student.age > 23 ? (
+//         <div className ="monowo" key = {student.id}>
+//             <div>Name: {student.name}</div>
+//             <div>Age: {student.age}</div>
+//             <div>Colour: {student.height}</div>
+//         </div>
+//         ) : null
+//     })
+//     return (
+//         <div className="student-list">{studentList}</div>
+//     )
+// }
+
+
     return (
-        <div className="student-list">{studentList}</div>
+        <div className="student-list">
+        {
+           students.map(student => {
+            return student.age > 23 ? (
+                <div className ="monowo" key = {student.id}>
+                    <div>Name: {student.name}</div>
+                    <div>Age: {student.age}</div>
+                    <div>Colour: {student.height}</div>
+                </div>
+                ) : null
+            })
+        }
+        </div>
     )
 }
 
