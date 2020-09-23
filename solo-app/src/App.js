140505446b5f96b62fba7e0369bc586a3f1552c6
Lekia <lekiagospel@gmail.com>
2020-09-23 18:13:26 +0100

    object list

--- solo-app/src/App.js
+++ solo-app/src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Monowo from './Monowo';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-      <Monowo name="James" age="41" colour="black"/>
-    </div>
-  );
+class App extends Component {
+  state = {
+    students: [
+      {id: 1, name: 'Eseosa', age: 30, height: '4 ft'},
+      {id: 2, name: 'Akpan', age: 24, height: '3.8 ft'},
+      {id: 3, name: 'Samuel', age: 19, height: '5.1 ft'}
+    ]
+  }
+    render() {
+    return (
+      <div className="App">
+        <h1>Hello</h1>
+        <Monowo students ={this.state.students} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- solo-app/src/Monowo.js
+++ solo-app/src/Monowo.js
@@ -1,15 +1,20 @@
 import React, {Component} from 'react';
 
 class Monowo extends Component {
-    render() {
-   const {name, age, colour} = this.props;
-        return (
-            <div className="monowo">
-                <div>Name: name</div>
-                <div>Age: {age}</div>
-                <div>Colour: {colour}</div>
-            </div>
-        )
+render() {
+   const {students} = this.props;
+   const studentList = students.map(student => {
+    return (   
+        <div className ="monowo" key = {student.id}>
+            <div>Name: {student.name}</div>
+            <div>Age: {student.age}</div>
+            <div>Colour: {student.height}</div>
+        </div>
+    )
+   })
+    return (
+        <div className="student-list">{studentList}</div>
+    )
     }
 }
 
