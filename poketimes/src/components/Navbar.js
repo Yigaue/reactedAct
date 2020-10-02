1a73f9e5c548f205e4cb8ceb3de4ea7134411d96
Lekia <lekiagospel@gmail.com>
2020-10-02 22:02:18 +0100

    router and redirects

--- poketimes/src/components/Navbar.js
+++ poketimes/src/components/Navbar.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import {Link, NavLink} from 'react-router-dom';
+import {Link, NavLink, withRouter} from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = (props) => {
+    setTimeout(() => {
+        props.history.push('/about')
+    }, 5000)
     return (
         <nav className="nav-wrapper red darken-3">
             <div className="container">
-                <a className="brand-logo">Poke'Times</a>
+                <a className="brand-logo" href="/">Poke'Times</a>
                 <ul className="right">
                     <li><Link to="/">Home</Link></li>
                     <li><NavLink to="/about">About Us</NavLink></li>
@@ -16,4 +19,4 @@ const Navbar = () => {
     )
 }
 
-export default Navbar;
+export default withRouter( Navbar);
