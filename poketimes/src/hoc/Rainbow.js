13df61b2fd4ad84ced2c9858421c7f2e596850d4
Lekia <lekiagospel@gmail.com>
2020-10-03 22:06:25 +0100

    super charged component

---
+++ poketimes/src/hoc/Rainbow.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Rainbow = (WrappedComponent) => {
+    const colours = ['red', 'pink', 'orange', 'blue', 'green', 'yellow'];
+    const randomColour = colours[Math.random() * 5];
+}
